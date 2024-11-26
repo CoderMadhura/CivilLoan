1adf3c8551946fa0133f3a883c03bd693185f04b
Madhura Taldeokar <madhurataldeokar@gmail.com>
2024-11-26 22:59:29 +0530

    Added CivilLoan Project

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 
-const mongoDB_URL=' mongodb://127.0.0.1:27017/CivilLoan';
+const mongoDB_URL='mongodb://127.0.0.1:27017/CivilLoan';
 
 mongoose.connect(mongoDB_URL,{
      useNewUrlParser:true,
